16de0858992b5c5f557e00bd80f91be4993413ad
Eric M Nunes Campelo <71527280+EricM1303@users.noreply.github.com>
2024-11-14 00:14:27 -0300

    :sparkles: feat: Incluindo lista ao código

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
+
+frutas = [
+    {
+        'id': 1,
+        'nome': 'maça',
+        'cor': 'vermelha',
+        'quantidade': 10
+    },
+    {
+        'id': 2,
+        'nome': 'banana',
+        'cor': 'amarela',
+        'quantidade': 5
+    },
+    {
+        'id': 3,
+        'nome': 'uva',
+        'cor': 'roxa',
+        'quantidade': 8
+    }
+]
